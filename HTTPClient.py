64b6e2ae532e3d022f140a2565a9d3e126565878
AveragelyIntelligentBird <anfisabogdanenko@gmail.com>
2024-03-24 20:36:44 -0400

    Implements HTTPS client and HTTP message parsing and send/receive functionality. HTML parser to go.

--- HTTPClient.py
+++ HTTPClient.py
@@ -1,5 +1,8 @@
+import logging
 import socket
 import ssl
+
+from HTTPResponse import HTTPResponse
 
 HTTP_VER = '1.1'
 MAX_PACKET_SIZE = 4096
@@ -13,27 +16,31 @@ class HTTPClient:
 
         self.cookies = {}  # Will contain sessionid and csrftoken cookies
 
-    def post_login(self, login_url, cgi_post_data):
-        encoded_cgi_post_data = cgi_post_data.encode(ENCODING)
+    def send_request(self, method, url, body=None):
+        if body:
+            request = self.__construct_request_header(method, url, len(body))
+            request += body
+        else:
+            request = self.__construct_request_header(method, url)
 
-        request = self.__construct_request_header('POST', login_url, len(encoded_cgi_post_data))
-        request += encoded_cgi_post_data
-        pass
-
-    def get_url(self, url):
-        request = self.__construct_request_header('GET', url)
-        print(request)
-
+        # Send request
+        logging.info(f"REQUEST:\n{request}")
         self.socket.send(request.encode(ENCODING))
-        data = self.socket.recv(MAX_PACKET_SIZE)
 
-        print("Response:\n%s" % data.decode(ENCODING))
-
-        # Covert response into a wrapper class
-
+        # Receive
+        response = self.__receive_response()
+        logging.info(f"RESPONSE:\n{response.get_header_str()}\n\n[BODY]")
+        logging.debug(response.get_body())
 
         # Update cookies if set-cookies
-        pass
+        new_cookies = response.get_new_cookies()
+        if new_cookies:
+            self.cookies.update(new_cookies)
+
+        return response
+
+    def close_connection(self):
+        self.socket.close()
 
     def __open_new_socket(self):
         context = ssl.create_default_context()
@@ -45,12 +52,12 @@ class HTTPClient:
 
     def __construct_request_header(self, method, url, content_length=0):
         header = f'{method} {url} HTTP/{HTTP_VER}\r\n'\
-                 f'Host : {self.server}:{self.port}\r\n'\
-                 'Connection: keep-alive'
+                 f'Host: {self.server}:{self.port}\r\n'\
+                 'Connection: keep-alive\r\n'
 
         if self.cookies:
             cookie_header_val = '; '.join(
-                [f'{key}={val}' for key, val in self.cookies]
+                [f'{key}={val}' for key, val in self.cookies.items()]
             )
             header += f'Cookie: {cookie_header_val}\r\n'
 
@@ -58,6 +65,87 @@ class HTTPClient:
             header += f'Content-Length: {content_length}\r\n'
 
         return header + '\r\n'
+
+    def __receive_response(self):
+        # Receive until header is fully received
+        buffer = b''
+        while b'\r\n\r\n' not in buffer:
+            data = self.socket.recv(MAX_PACKET_SIZE)
+            if not data:  # Socket closed too soon
+                return HTTPResponse("", 503, "Local - Socket closed before the end of packet, retry.")
+            buffer += data
+
+        # Process header to determine remaining length of content
+        header_string, _, body_part = buffer.partition(b'\r\n\r\n')
+        response = HTTPResponse(header_string.decode(ENCODING))
+
+        # Get remaining response data by content length
+        content_len = response.get_content_length()
+        if content_len is not None:
+            complete_body = self.__receive_body_by_content_len(body_part, content_len)
+        elif response.is_transfer_encoding_chunked(): # Alternatively, consider chunked encoding
+            complete_body = self.__receive_chunked_body(body_part)
+        else:
+            complete_body = ""
+
+        # Socket closed too soon, report a 503 for the crawler to retry
+        if complete_body is None:
+            return HTTPResponse("", 503, "Local - Socket closed before the end of packet, retry.")
+
+        response.set_body(complete_body.decode(ENCODING))
+
+        # Account for whether the server closed connection with this packet
+        # If so, simply reopen socket
+        if response.is_connection_closed():
+            self.close_connection()
+            self.__open_new_socket()
+
+        return response
+
+    def __receive_body_by_content_len(self, init_body_part, content_len):
+        rem_content_len = content_len - len(init_body_part)
+        body_chunks = [init_body_part]
+
+        # Receive data for the content body for as long as there is outstanding content length
+        while rem_content_len > 0:
+            body_chunk = self.socket.recv(MAX_PACKET_SIZE)
+            if not body_chunk:  # Socket closed
+                return None
+            rem_content_len -= len(body_chunk)
+            body_chunks.append(body_chunk)  # Appending to list instead of to string for speed
+
+        return b''.join(body_chunks)
+
+    def __receive_chunked_body(self, body_buf):
+        # Receive the rest of the packet, expecting to have CRLFx2 in the end
+        while b'\r\n\r\n' not in body_buf:
+            data = self.socket.recv(MAX_PACKET_SIZE)
+            if not data:  # Socket closed
+                return None
+            body_buf += data
+
+        # Parse the chunks from the buffer until a chunk-size of 0 is encountered
+        body_chunks = []
+        expecting_chunk_len = True
+        next_chunk_len = -1  # -1 for expecting to read chunk len on the next line
+        while next_chunk_len:
+            if b'\r\n' in body_buf:
+                if expecting_chunk_len:
+                    chunk_len_line, _, body_buf = body_buf.partition(b'\r\n')
+
+                    next_chunk_len = int(chunk_len_line.split(b';')[0], 16)
+                    expecting_chunk_len = False
+                else:
+                    chunk_line, _, body_buf = body_buf.partition(b'\r\n')
+                    if len(chunk_line) == next_chunk_len:
+                        body_chunks.append(chunk_line)
+                        expecting_chunk_len = True
+                        next_chunk_len = -1
+                    else:
+                        logging.error("Malformed chunk was received. Retrying the same transmission.")
+                        return None
+
+        return b''.join(body_chunks)
 
 
 class HTTPUtils:
@@ -67,5 +155,5 @@ class HTTPUtils:
                       "csrfmiddlewaretoken" : csrfmiddlewaretoken,
                       "next" : "" }
         return '&'.join(
-                [f'{key}={val}' for key, val in cgi_fields]
+                [f'{key}={val}' for key, val in cgi_fields.items()]
             )
--- HTTPResponse.py
+++ HTTPResponse.py
@@ -1,2 +1,98 @@
-class HTTPResponseHeader:
-    pass
+
+class HTTPResponse:
+    header_lines = {}
+    body = ""
+
+    def __init__(self, header_string, response_code=None, response_message=None):
+        # If the message was constructed manually
+        if response_code and response_message:
+            self.response_code = response_code
+            self.header_string = f"HTTP/1.1 {response_code} {response_message}\n\n"
+        else:
+            self.header_string = header_string
+            self.response_code = self.__parse_header(header_string)
+
+    def get_header_str(self):
+        return self.header_string
+
+    def set_body(self, body):
+        self.body = body
+
+    def get_body(self):
+        return self.body
+
+    def get_response_code(self):
+        return self.response_code
+
+    def is_connection_closed(self):
+        return ('connection' in self.header_lines and self.header_lines['connection'][0].strip() == "close") \
+            or ('Connection' in self.header_lines and self.header_lines['Connection'][0].strip() == "close")
+
+    def get_new_cookies(self):
+        if 'set-cookie' in self.header_lines:
+            new_cookie_header_vals = self.header_lines['set-cookie']
+        elif 'Set-Cookie' in self.header_lines:
+            new_cookie_header_vals = self.header_lines['Set-Cookie']
+        elif 'Set-cookie' in self.header_lines:
+            new_cookie_header_vals = self.header_lines['Set-cookie']
+        else:
+            return None
+
+        new_cookies = {}
+        for new_cookie_header_val in new_cookie_header_vals:
+            cookie_name, cookie_val = new_cookie_header_val.split(';', 1)[0].strip().split("=", 1)
+            new_cookies[cookie_name] = cookie_val
+
+        return new_cookies
+
+    def get_content_length(self):
+        if 'content-length' in self.header_lines:
+            content_len_val = self.header_lines['content-length'][0]
+        elif 'Content-Length' in self.header_lines:
+            content_len_val = self.header_lines['Content-Length'][0]
+        elif 'Content-length' in self.header_lines:
+            content_len_val = self.header_lines['Content-length'][0]
+        else:
+            return None
+
+        return int(content_len_val.strip())
+
+    def is_transfer_encoding_chunked(self):
+        return ('transfer-encoding' in self.header_lines
+                and self.header_lines['Transfer-Encoding'][0].strip() == "chunked") \
+            or ('Transfer-Encoding' in self.header_lines
+                and self.header_lines['Transfer-Encoding'][0].strip() == "chunked") \
+            or ('Transfer-encoding' in self.header_lines
+                and self.header_lines['Transfer-encoding'][0].strip() == "chunked")
+
+    def get_new_location(self):
+        if 'location' in self.header_lines:
+            location_val = self.header_lines['location'][0]
+        elif 'Location' in self.header_lines:
+            location_val = self.header_lines['Location'][0]
+        else:
+            return None
+
+        return location_val.strip()
+
+    def __parse_header(self, header_string):
+        header_lines = header_string.split("\r\n")
+
+        if header_lines:
+            response_code = int(header_lines[0].split(" ")[1])
+        else:
+            raise RuntimeError("The received header string was empty.")
+
+        if len(header_lines) > 1:
+            header_lines = header_lines[1:]
+            for line in header_lines:
+                if not line:
+                    continue
+
+                key, value = line.split(':', 1)
+                if key in self.header_lines:
+                    self.header_lines[key].append(value)
+                else:
+                    self.header_lines[key] = [value]
+
+        return response_code
